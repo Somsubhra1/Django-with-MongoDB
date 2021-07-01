442087bd473164b890ce9b9fc5dc7fd9038147c3
Somsubhra Das <das.somsubhra1@gmail.com>
2021-07-01 12:28:47 +0530

    blog serializer created

---
+++ blog/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Blog
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
